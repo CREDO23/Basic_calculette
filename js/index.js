7d9eac457d0579ef902279d63decc6ddc4c25156
CREDO23 <thierrybakera12@gmail.com>
2022-06-29 12:31:18 +0200

    fin projet

--- js/index.js
+++ js/index.js
@@ -3,11 +3,28 @@ const buttons = document.querySelectorAll('input');
 
 const scren = document.querySelector('h1');
 
-const regex = /^[\()\*\/\-%+0-9]{1,10}$/;
+const regex = /^[\()\*\/\-%.+0-9]{1,10}$/;
+
+const isBeginingByZero = (param) => {
+	let str = param;
+	while (str[0] == '0' && /[0-9]/.test(str[1])) {
+		str = str.slice(1);
+	}
+	return str;
+};
+
+const sizeFont = () => {
+	if (scren.textContent.length > 10) {
+		scren.classList.add('mediumSize');
+	} else {
+		scren.classList.remove('mediumSize');
+	}
+};
 
 buttons.forEach((button) => {
 	button.addEventListener('click', (e) => {
 		scren.textContent += e.target.value;
+		sizeFont();
 	});
 });
 
@@ -16,20 +33,22 @@ clear.addEventListener('click', () => {
 });
 
 const result = (str) => {
+	let x = isBeginingByZero(str);
 	if (regex.test(str)) {
-		return Function(`return ${str}`)();
+		return Function(`return ${x}`)();
 	}
 };
 
 equal.addEventListener('click', () => {
 	try {
-		result(scren.textContent);
-		scren.textContent = result(scren.textContent);
+		scren.textContent = parseFloat(result(scren.textContent));
+		sizeFont();
 	} catch (e) {
-		scren.textContent = 'Nan';
+		scren.textContent = 'NaN';
 	}
 });
 
 del.addEventListener('click', () => {
+	sizeFont();
 	scren.textContent = scren.textContent.slice(0, -1);
 });
